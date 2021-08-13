07dce7492f8cba487acfc5777ff8cdd8345ef668
Bruno Henrique Gonçalves <goncalves.brunohenrique@gmail.com>
2021-08-13 09:07:25 -0300

    correção dependências

--- FormPrincipal.cs
+++ FormPrincipal.cs
@@ -1,14 +1,7 @@
-﻿using System;
-using System.Collections.Generic;
-using System.ComponentModel;
-using System.Data;
-using System.Drawing;
+﻿using EntityFrameworkExemplo1.Repository;
+using System;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows.Forms;
-using System.Data.Entity;
-using EntityFrameworkExemplo1.Repository;
 
 namespace EntityFrameworkExemplo1
 {
